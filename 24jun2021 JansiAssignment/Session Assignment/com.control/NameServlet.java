f264fa9a78bc374da9c0fc05a1610d26a863f4fd
Janakiram46 <pamarla.janakiram@thirdware.com>
2021-06-28 08:55:50 +0530

    Janakiram MentorAssigment

---
+++ 24jun2021 JansiAssignment/Session Assignment/com.control/NameServlet.java
@@ -0,0 +1,58 @@
+package com.Practice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class NameServlet
+ */
+@WebServlet("/NameServlet")
+public class NameServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NameServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append(request.getContextPath());
+		 PrintWriter out = response.getWriter();  
+	        HttpSession session=request.getSession();  
+	        
+		   String n1=  (String) session.getAttribute("FirstName"); 
+		   String n2=  (String) session.getAttribute("LastName"); 
+		   System.out.println("Hello "+n1+" "+n2);
+		   
+	        out.print("Hello "+n1+" "+n2);  
+		     out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		PrintWriter out=response.getWriter();
+		
+		
+		
+	}
+
+}
---
+++ 24jun2021 JansiAssignment/Session Assignment/com.view/Naming.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="SessionServlet" >
+ <table>
+ <tr>
+          <th>First Name</th>
+          <td><input type="text" name="first"></td>
+        </tr>
+          <th>Last Name</th>
+          <td><input type="text" name="last"></td>
+        </tr>
+         <td><input type="submit" value="Submit" class="btn btn-primary"></td>
+ </table>
+ </form>
+</body>
+</html>
